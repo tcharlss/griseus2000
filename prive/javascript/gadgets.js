8e92c78d3e305aa51ec266afe8cffb27b49bd130
Matthieu Marcillaud <marcimat@rezo.net>
2017-12-16 21:36:40 +0100

    Refonte du JS pour le simplifier et ne plus avoir à gérer de .actif_tempo ni de temporisation. De nos jours on doit pouvoir utiliser une technique avec :before pour que le «hover» dépasse le menu, ce qui fait que si la souris déborde un peu, le menu ne se ferme pas.

--- prive/javascript/gadgets.js
+++ prive/javascript/gadgets.js
@@ -1,74 +1,42 @@
-function init_gadgets(url_menu_rubrique){
-	jQuery('#boutonbandeautoutsite').one('mouseover',function(){
-		jQuery(this).siblings('ul').find('li:first>a').animeajax();
-		jQuery.ajax({
-			url: url_menu_rubrique,
-			success: function(c){
-				jQuery('#boutonbandeautoutsite').siblings('ul').remove();
-				jQuery('#boutonbandeautoutsite')
-					.after(c)
-					.parent().find('li').menuFocus();
-			}
-		});
-	});
-}
 function focus_zone(selecteur){
 	jQuery(selecteur).eq(0).find('a,input:visible').get(0).focus();
 	return false;
 }
-jQuery(function(){
+
+jQuery(function($){
+	function init_gadgets(url_menu_rubrique){
+		$('#boutonbandeautoutsite').one('mouseover',function(){
+			$(this).siblings('ul').find('li:first>a').animeajax();
+			$.ajax({
+				url: url_menu_rubrique,
+				success: function(c){
+					$('#boutonbandeautoutsite').siblings('ul').remove();
+					$('#boutonbandeautoutsite').after(c).parent().find('li').menuFocus();
+				}
+			});
+		}).one('focus touchend', function(){
+			$(this).trigger('mouseover');
+		});
+	}
+
 	init_gadgets(url_menu_rubrique);
-	var is_open = 0;
-	jQuery.fn.menuItemOpen = function(){
-		jQuery(this)
-			.addClass('actif')
-			.parents('li').addClass('actif');
-		jQuery(this).siblings('li').removeClass('actif_tempo');
-		is_open = true;
-		return this;
-	}
-	jQuery.fn.menuItemClose = function(){
-		jQuery(this)
-			.removeClass('actif_tempo');
-		is_open = (jQuery(this).parents('ul').eq(-1).find('li.actif').length>0);
-		return this;
-	}
+
 	// deplier le menu au focus clavier,
 	// enlever ce depliement si passage a la souris,
 	// delai de fermeture.
-	jQuery.fn.menuFocus = function(){
-		jQuery(this)
-		// le replier si un hover de souris sur un autre onglet,
-		// timer sur la fermeture des onglets pour ne pas que ca aille trop vite
-		// timer sur l'ouverture des onglets pour ne tolerer les derapages
-			.on('mouseenter',
-				function(){
-					if (this.timerout)
-						clearTimeout(this.timerout);
-					this.timerout = null;
-					this.timerin = null;
-					if (is_open)
-						jQuery(this).menuItemOpen();
-					else {
-						var me = jQuery(this);
-						this.timerin= setTimeout(function(){
-							me.menuItemOpen(null);
-						}, 200);
-					}
-				})
-			.on('mouseleave',
-				function(){
-					if (this.timerin)
-						clearTimeout(this.timerin);
-					this.timerin = null;
-					if (is_open){
-						var me = jQuery(this).removeClass('actif').addClass('actif_tempo');
-						this.timerout = setTimeout(function(){
-							me.menuItemClose();
-						}, 400);
-					}
-				}
-			)
+	$.fn.menuFocus = function(){
+		var $racine = $(this);
+		var $deroulants = $racine.find('.deroulant');
+
+		$deroulants.find('li')
+			// le replier si un hover de souris sur un autre onglet,
+			.on('mouseenter', function(){
+				var $parents_and_me = $(this).parents('li').add($(this)).addClass('actif');
+				$deroulants.find('.actif').not($parents_and_me).removeClass('.actif');
+			})
+			.on('mouseleave', function(){
+				$(this).removeClass('actif');
+			})
 			// navigation au doigt des items déroulants
 			.has('ul').find(' > a')
 			.on('touchend', function(event) {
@@ -83,41 +51,22 @@ jQuery(function(){
 			})
 			.end().end()
 			.find('> a, li > a')
-			// navigation au clavier :
-			// deplier le ul enfant
+			// navigation au clavier : deplier le ul enfant
 			.on('focus', function(){
-				jQuery(this).parents('li').siblings('.actif').removeClass('actif');
-				jQuery(this).parents('li').addClass('actif');
-			})
-			// cacher en partant de l'onglet...
-			.on('blur', function(){
-				jQuery(this).parents('li').removeClass('actif');
+				var $parents = $(this).parents('li').addClass('actif');
+				$deroulants.find('.actif').not($parents).removeClass('actif');
 			});
 		return this;
 	}
 
-	// Controler la position verticale des sous-menus
-	// pour l'instant, effectuer a chaque hover, en cas de changement de taille d'affichage par exemple
-	jQuery('#bando_navigation').on('hover touchstart', function(){
-		hauteur = parseInt(jQuery('#bando_navigation .largeur').height())
-			+  parseInt(jQuery('#bando_navigation').css("padding-top"))
-			+  parseInt(jQuery('#bando_navigation').css("padding-bottom"));
-		jQuery('#bando_navigation ul li>ul').css({'top':hauteur});
-	});
-
-	jQuery('#bando_navigation .deroulant > li').menuFocus();
-	jQuery('#bando_principal .deroulant li').menuFocus();
-
-	jQuery('#bandeau_haut #formRecherche input').on('hover touchstart', function(){
-		jQuery('#bandeau_haut ul.actif').trigger('mouseout');
-	});
-	jQuery('#bando_liens_rapides a')
-		.on('focus', function(){
-			jQuery('#bando_liens_rapides').addClass('actif');
-		})
-		.on('blur', function(){
-			jQuery('#bando_liens_rapides').removeClass('actif');
+	$('#bando_haut')
+		.menuFocus()
+		// le focus de certains éléments doit replier les menus dépliés
+		.find('#bando_rechercher input, #bando_liens_rapides a, #bando_site a')
+		.on('focus touchstart', function() {
+			$('#bando_haut').find('.deroulant .actif').removeClass('actif');
 		});
+
 	if (typeof window.test_accepte_ajax != "undefined") {
 		test_accepte_ajax();
 	}
