c651d7d61f8000ff87ced0b88acf0cff794cdbad
Charles Razack <charles@ldd.fr>
2017-12-17 20:19:57 +0100

    Entrée dans le menu dev

---
+++ lang/griseus2000_fr.php
@@ -0,0 +1,12 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// T
+	'titre_menu' => 'Griseus2000',
+);
